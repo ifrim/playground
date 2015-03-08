ae65bdd40dc15a451e063edf64ae681a40421160
ifrim <vlad27aug@yahoo.com>
2015-03-08 18:11:20 +0200

    testing async/await, with browserify and babelify

---
+++ ESnext/async-await.js
@@ -0,0 +1,22 @@
+var keys = ['k1', 'k3', 'k4'];
+var people = {
+	k1: {firstName: 'Mihai', lastName: 'Eminescu'},
+	k2: {firstName: 'George', lastName: 'Enescu'},
+	k3: {firstName: 'Nicolae', lastName: 'Iorga'},
+	k4: {firstName: 'Stefan', lastName: 'Luchian'},
+	k5: {firstName: 'Octav', lastName: 'Onicescu'},
+	k6: {firstName: 'Grigore', lastName: 'Antipa'}
+    };
+var getKeys = () => new Promise(resolve => setTimeout(() => resolve(keys), 3000));
+var getPerson = (key) => new Promise(resolve => setTimeout(() => resolve(people[key]), 3000));
+
+(async function() {
+    var keysForPeople = await getKeys();
+
+    console.log('>>', keysForPeople);
+
+    for(let person of keysForPeople.map(getPerson)) {
+	let p = await person;
+	console.log(p.firstName + ' ' + p.lastName);
+    }
+}());
---
+++ ESnext/readme.md
@@ -0,0 +1,2 @@
+run this command to generate the bundle file:
+browserify async-await.js -t [babelify --optional runtime --experimental] -o async-await-bundle.js
