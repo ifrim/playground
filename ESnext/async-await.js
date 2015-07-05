31120c3d827ac108df6f7afd6934262fa9fe6412
ifrim <vlad27aug@yahoo.com>
2015-07-05 16:53:15 +0300

    very small changes

--- ESnext/async-await.js
+++ ESnext/async-await.js
@@ -1,14 +1,14 @@
 var keys = ['k1', 'k3', 'k4'];
 var people = {
-	k1: {firstName: 'Mihai', lastName: 'Eminescu'},
-	k2: {firstName: 'George', lastName: 'Enescu'},
-	k3: {firstName: 'Nicolae', lastName: 'Iorga'},
-	k4: {firstName: 'Stefan', lastName: 'Luchian'},
-	k5: {firstName: 'Octav', lastName: 'Onicescu'},
-	k6: {firstName: 'Grigore', lastName: 'Antipa'}
+    	k1: {firstName: 'Mihai', lastName: 'Eminescu'},
+    	k2: {firstName: 'George', lastName: 'Enescu'},
+    	k3: {firstName: 'Nicolae', lastName: 'Iorga'},
+    	k4: {firstName: 'Stefan', lastName: 'Luchian'},
+    	k5: {firstName: 'Octav', lastName: 'Onicescu'},
+    	k6: {firstName: 'Grigore', lastName: 'Antipa'}
     };
 var getKeys = () => new Promise(resolve => setTimeout(() => resolve(keys), 3000));
-var getPerson = (key) => new Promise(resolve => setTimeout(() => resolve(people[key]), 3000));
+var getPerson = key => new Promise(resolve => setTimeout(() => resolve(people[key]), 3000));
 
 var asyncFn = (async function() {
     var keysForPeople = await getKeys();
@@ -16,8 +16,8 @@ var asyncFn = (async function() {
     console.log('>>', keysForPeople);
 
     for(let person of keysForPeople.map(getPerson)) {
-	let p = await person;
-	console.log(p.firstName + ' ' + p.lastName);
+		let p = await person;
+		console.log(p.firstName + ' ' + p.lastName);
     }
 
     return 'async function terminated';
@@ -26,8 +26,8 @@ var asyncFn = (async function() {
 console.log('asyncFn>', asyncFn);
 asyncFn.then(data => console.log('asyncFn data>', data));
 
-var getEminescu = async function() { 
-    return await getPerson((await getKeys())[0]);
+var getEminescu = async function() {
+    return await getPerson( (await getKeys())[0] );
 };
 var eminescu = getEminescu();
 
